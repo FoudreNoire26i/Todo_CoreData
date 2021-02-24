1141bc2a04299668f36210b9f3da3f10b4db1d22
p1807300 <benjamin.vicat@etu.univ-lyon1.fr>
2021-02-24 09:16:11 +0100

    Initial Commit

---
+++ Todo_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo_CoreData
+//
+//  Created by lpiem2 on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
